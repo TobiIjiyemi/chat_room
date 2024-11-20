4ddefc327d2d905e1368403b67c81b15de47c40a
Tobi Ijiyemi <tobi.ijiyemi@gmail.com>
2024-11-19 21:35:30 -0500

    changed hosting url to render generated url

--- public/script.js
+++ public/script.js
@@ -1,18 +1,16 @@
-const socket = io("http://localhost:4000");
-// socket.on("messageFromServer", data => {
-//     console.log(data);
-//     socket.emit("messageToServer", {data: socket.id + ", just connected!"});
-// } );
+const socket = io("https://chat-room-xyan.onrender.com/");
 
 const form = document.getElementById("formInput");
 form.addEventListener('submit', e => {
     e.preventDefault();
     const message = document.getElementById("messageInput").value;
-    socket.emit("chatToServer", {message}); //send message to server
+    document.getElementById("messageInput").value = "";
+    socket.emit("chatToServer", 
+        socket.id + " typed: " + message
+    ); //send message to server
 });
 
 socket.on("chatToClient", dataFromServer => {
     let messages = document.getElementById("messages");
-    console.log(dataFromServer);
-    messages.innerHTML += `<li>${socket.id + ": " + dataFromServer.message}</li>`;
+    messages.innerHTML += `<li>${dataFromServer}</li>`;
 });
