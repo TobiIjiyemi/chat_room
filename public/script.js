b28fe2377dffe0322bd52f0c4bb809ab394dfd06
Tobi Ijiyemi <tobi.ijiyemi@gmail.com>
2024-11-19 19:10:36 -0500

    attempted to chat room. create listeners on the input button and sending a message through chatToServer and receiving through chatToClient

--- public/script.js
+++ public/script.js
@@ -1,5 +1,18 @@
 const socket = io("http://localhost:4000");
-socket.on("messageFromServer", data => {
-    console.log(data);
-    socket.emit("messageToServer", {data: socket.id + ", just connected!"});
-} );
+// socket.on("messageFromServer", data => {
+//     console.log(data);
+//     socket.emit("messageToServer", {data: socket.id + ", just connected!"});
+// } );
+
+const form = document.getElementById("formInput");
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = document.getElementById("messageInput").value;
+    socket.emit("chatToServer", {message}); //send message to server
+});
+
+socket.on("chatToClient", dataFromServer => {
+    let messages = document.getElementById("messages");
+    console.log(dataFromServer);
+    messages.innerHTML += `<li>${socket.id + ": " + dataFromServer.message}</li>`;
+});
--- server.js
+++ server.js
@@ -15,9 +15,13 @@ io.on('connection', socket => {
     //for each connection output "Welcome to the socketio server"
     socket.emit("messageFromServer", {data: "Welcome to the socketio server"});
 
-    //listen for messageToServer, see what each socket wants to send back
-    socket.on("messageToServer", dataFromClient => {
-        console.log(dataFromClient);
-    });
+    // //listen for messageToServer, see what each socket wants to send back
+    // socket.on("messageToServer", dataFromClient => {
+    //     console.log(dataFromClient);
+    // });
+    
+    socket.on("chatToServer", dataFromClient => {
+        io.emit("chatToClient", dataFromClient);
+     });
 });
 
